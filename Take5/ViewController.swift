da6946ac44cc7909bea430f59f484506b7dc7f94
harinwu <master.harin@gmail.com>
2019-01-10 14:08:49 -0800

    Initial Commit

---
+++ Take5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Take5
+//
+//  Created by Harin Wu on 2019-01-10.
+//  Copyright © 2019 Hao Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
