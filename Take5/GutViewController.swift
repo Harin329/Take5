b6de6c513c9b30f1d7be7d94517724fa9f03e4ba
harinwu <master.harin@gmail.com>
2019-01-13 21:58:30 -0800

    looks bad, but functional!

--- Take5/GutViewController.swift
+++ Take5/GutViewController.swift
@@ -11,11 +11,15 @@ import UIKit
 class GutViewController: UIViewController {
     @IBOutlet weak var Option1: UIButton!
     @IBOutlet weak var Option2: UIButton!
+    @IBOutlet weak var timeLeft: UILabel!
+    var count: Int = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
         Option1.setTitle(choice1, for: .normal)
         Option2.setTitle(choice2, for: .normal)
+        _ = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GutViewController.update), userInfo: nil, repeats: true)
+        
         // Do any additional setup after loading the view.
     }
     @IBAction func gut1(_ sender: UIButton) {
@@ -24,6 +28,16 @@ class GutViewController: UIViewController {
     @IBAction func gut2(_ sender: UIButton) {
         second += 5
     }
+    
+    @objc func update() {
+        if(count > 0) {
+            count -= 1
+            timeLeft.text = String(count)
+        } else {
+             self.performSegue(withIdentifier: "finish", sender: self)
+        }
+    }
+    
 
     /*
     // MARK: - Navigation
--- Take5/ProConViewController.swift
+++ Take5/ProConViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class ProConViewController: UIViewController {
+class ProConViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var Option1: UILabel!
     @IBOutlet weak var Option2: UILabel!
     @IBOutlet weak var text1: UITextField!
     @IBOutlet weak var text2: UITextField!
-    @IBOutlet weak var table1: UITableView!
+    @IBOutlet weak var Pro1: UIButton!
+    @IBOutlet weak var Con1: UIButton!
+    @IBOutlet weak var Pro2: UIButton!
+    @IBOutlet weak var Con2: UIButton!
+    @IBOutlet weak var result1: UITextView!
+    @IBOutlet weak var result2: UITextView!
+    
+    var oneEdit: Int = 0
+    var twoEdit: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,11 +29,63 @@ class ProConViewController: UIViewController {
         Option2.text = choice2
         text1.placeholder = "Pros or Cons for " + choice1
         text2.placeholder = "Pros or Cons for " + choice2
+        text1.delegate = self
+        text2.delegate = self
         // Do any additional setup after loading the view.
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     
-
+    @IBAction func Pro1(_ sender: UIButton) {
+        if let text = text1.text {
+            if text == "" {
+                return
+            }
+            result1.text.append("++" + "\(text)\n")
+            text1.text = ""
+            text1.resignFirstResponder()
+            first += 1
+        }
+    }
+    
+    @IBAction func Con1(_ sender: UIButton) {
+        if let text = text1.text {
+            if text == "" {
+                return
+            }
+            result1.text.append("--" + "\(text)\n")
+            text1.text = ""
+            text1.resignFirstResponder()
+            first -= 1
+        }
+    }
+    
+    @IBAction func Pro2(_ sender: UIButton) {
+        if let text = text2.text {
+            if text == "" {
+                return
+            }
+            result2.text.append("++" + "\(text)\n")
+            text2.text = ""
+            text2.resignFirstResponder()
+            second += 1
+        }
+    }
+    
+    @IBAction func Con2(_ sender: UIButton) {
+        if let text = text2.text {
+            if text == "" {
+                return
+            }
+            result2.text.append("--" + "\(text)\n")
+            text2.text = ""
+            text2.resignFirstResponder()
+            second -= 1
+        }
+    }
     /*
     // MARK: - Navigation
 
